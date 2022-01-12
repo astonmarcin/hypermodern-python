4e09c34313a5a89ee0312dbec6e87a88e0ff30aa
Aston Marcin <a@b.com>
2022-01-12 16:45:31 +0100

    pytest-mock

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,17 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
-    assert result.exit_code == 0
+    #assert result.exit_code == 0
+    #assert "Lorem Ipsum" in result.output
+    assert mock_requests_get.called
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
